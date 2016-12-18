06f90e18f913038c6dbdd914fe9e6e47c1334eff
Michael Xu <michaeljxu11@gmail.com>
2016-12-17 22:14:15 -0800

    split into interface and AI layer

--- content.js
+++ content.js
@@ -1,9 +1,20 @@
+function injectGameInterfaceLayer() {
+  console.log("injecting game interface layer");
+  var s = document.createElement('script');
+  s.src = chrome.extension.getURL('generals-interface.js');
+  s.onload = function() {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(s);
+}
+
 function injectGameAI() {
   console.log("injecting game UI");
   var s = document.createElement('script');
   s.src = chrome.extension.getURL('generals-ai.js');
   s.onload = function() {
     this.remove();
+    injectGameInterfaceLayer();
   };
   (document.head || document.documentElement).appendChild(s);
 }
